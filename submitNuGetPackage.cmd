08b733711e23dbdb0b2b20287183013580c42f7e
Florian Engel <florian.engel@qaware.de>
2023-04-21 08:21:11 +0200

    Remove PowerShell build scripts and tooling
    We move package builds to the CI, so there is
    no need for keeping executables around which we'll have
    to maintain and update.

--- submitNuGetPackage.cmd
+++
@@ -1,7 +0,0 @@
-CALL buildNuGetPackage.cmd
-
-@ECHO OFF
-FOR /F "delims=|" %%I IN ('DIR "Jace.*.nupkg" /B /O:D') DO SET NuGetPackage=%%I
-@ECHO ON
-
-Tools\NuGet\nuget.exe push %NuGetPackage% -Source https://api.nuget.org/v3/index.json
