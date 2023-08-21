4a1b76ff6fe07dea1c065b995268e727b491ec1e
Florian Engel <florian.engel@adletec.de>
2023-08-21 15:00:54 +0200

    Extract interface for CalculationEngine.

--- Jace/CalculationEngine.cs
+++ Jace/CalculationEngine.cs
@@ -11,12 +11,8 @@ namespace Jace
 {
     public delegate TResult DynamicFunc<T, TResult>(params T[] values);
 
-    /// <summary>
-    /// The CalculationEngine class is the main class of Jace.NET to convert strings containing
-    /// mathematical formulas into .NET Delegates and to calculate the result.
-    /// It can be configured to run in a number of modes based on the constructor parameters chosen.
-    /// </summary>
-    public class CalculationEngine
+    /// <inheritdoc/>
+    public class CalculationEngine : ICalculationEngine
     {
         private readonly IExecutor executor;
         private readonly Optimizer optimizer;
@@ -28,70 +24,27 @@ namespace Jace
 
         private readonly Random random;
 
-        /// <summary>
-        /// Creates a new instance of the <see cref="CalculationEngine"/> class with
-        /// default parameters.
-        /// </summary>
         public CalculationEngine()
             : this(new JaceOptions())
         {
         }
 
-        /// <summary>
-        /// Creates a new instance of the <see cref="CalculationEngine"/> class. The dynamic compiler
-        /// is used for formula execution and the optimizer and cache are enabled.
-        /// </summary>
-        /// <param name="cultureInfo">
-        /// The <see cref="CultureInfo"/> required for correctly reading floating point numbers.
-        /// </param>
         public CalculationEngine(CultureInfo cultureInfo)
             : this(new JaceOptions { CultureInfo = cultureInfo })
         {
         }
 
-        /// <summary>
-        /// Creates a new instance of the <see cref="CalculationEngine"/> class. The optimizer and 
-        /// cache are enabled.
-        /// </summary>
-        /// <param name="cultureInfo">
-        /// The <see cref="CultureInfo"/> required for correctly reading floating point numbers.
-        /// </param>
-        /// <param name="executionMode">The execution mode that must be used for formula execution.</param>
         public CalculationEngine(CultureInfo cultureInfo, ExecutionMode executionMode)
             : this (new JaceOptions { CultureInfo = cultureInfo, ExecutionMode = executionMode })
         {
         }
 
-        /// <summary>
-        /// Creates a new instance of the <see cref="CalculationEngine"/> class.
-        /// </summary>
-        /// <param name="cultureInfo">
-        /// The <see cref="CultureInfo"/> required for correctly reading floating point numbers.
-        /// </param>
-        /// <param name="executionMode">The execution mode that must be used for formula execution.</param>
-        /// <param name="cacheEnabled">Enable or disable caching of mathematical formulas.</param>
-        /// <param name="optimizerEnabled">Enable or disable optimizing of formulas.</param>
-        /// <param name="adjustVariableCaseEnabled">Enable or disable auto lower-casing of variables.</param>
         [Obsolete]
         public CalculationEngine(CultureInfo cultureInfo, ExecutionMode executionMode, bool cacheEnabled, bool optimizerEnabled, bool adjustVariableCaseEnabled)
             : this(new JaceOptions { CultureInfo = cultureInfo, ExecutionMode = executionMode, CacheEnabled = cacheEnabled, OptimizerEnabled = optimizerEnabled, CaseSensitive = !adjustVariableCaseEnabled })
         {
         }
 
-        /// <summary>
-        /// Creates a new instance of the <see cref="CalculationEngine"/> class.
-        /// </summary>
-        /// <param name="cultureInfo">
-        /// The <see cref="CultureInfo"/> required for correctly reading floating point numbers.
-        /// </param>
-        /// <param name="executionMode">The execution mode that must be used for formula execution.</param>
-        /// <param name="cacheEnabled">Enable or disable caching of mathematical formulas.</param>
-        /// <param name="optimizerEnabled">Enable or disable optimizing of formulas.</param>
-        /// <param name="adjustVariableCaseEnabled">Enable or disable converting to lower case.</param>
-        /// <param name="defaultFunctions">Enable or disable the default functions.</param>
-        /// <param name="defaultConstants">Enable or disable the default constants.</param>
-        /// <param name="cacheMaximumSize">Configure the maximum cache size for mathematical formulas.</param>
-        /// <param name="cacheReductionSize">Configure the cache reduction size for mathematical formulas.</param>
         [Obsolete]
         public CalculationEngine(CultureInfo cultureInfo, ExecutionMode executionMode, bool cacheEnabled,
             bool optimizerEnabled, bool adjustVariableCaseEnabled, bool defaultFunctions, bool defaultConstants, int cacheMaximumSize, int cacheReductionSize)
@@ -102,10 +55,6 @@ namespace Jace
         {
         }
 
-        /// <summary>
-        /// Creates a new instance of the <see cref="CalculationEngine"/> class.
-        /// </summary>
-        /// <param name="options">The <see cref="JaceOptions"/> to configure the behaviour of the engine.</param>
         public CalculationEngine(JaceOptions options)
         {
             this.caseSensitive = options.CaseSensitive;
@@ -133,11 +82,11 @@ namespace Jace
 
             optimizer = new Optimizer(new Interpreter()); // We run the optimizer with the interpreter 
 
-            // Register the default constants of Jace.NET into the constant registry
+            // Register the default constants of jace into the constant registry
             if (options.DefaultConstants)
                 RegisterDefaultConstants();
 
-            // Register the default functions of Jace.NET into the function registry
+            // Register the default functions of jace into the function registry
             if (options.DefaultFunctions)
                 RegisterDefaultFunctions();
         }
@@ -191,11 +140,6 @@ namespace Jace
             return new FormulaBuilder(formulaText, caseSensitive, this);
         }
 
-        /// <summary>
-        /// Build a .NET func for the provided formula.
-        /// </summary>
-        /// <param name="formulaText">The formula that must be converted into a .NET func.</param>
-        /// <returns>A .NET func for the provided formula.</returns>
         public Func<IDictionary<string, double>, double> Build(string formulaText)
         {
             if (string.IsNullOrEmpty(formulaText))
@@ -210,12 +154,6 @@ namespace Jace
             return BuildFormula(formulaText, null, operation);
         }
 
-        /// <summary>
-        /// Build a .NET func for the provided formula.
-        /// </summary>
-        /// <param name="formulaText">The formula that must be converted into a .NET func.</param>
-        /// <param name="constants">Constant values for variables defined into the formula. They variables will be replaced by the constant value at pre-compilation time.</param>
-        /// <returns>A .NET func for the provided formula.</returns>
         public Func<IDictionary<string, double>, double> Build(string formulaText, IDictionary<string, double> constants)
         {
             if (string.IsNullOrEmpty(formulaText))
@@ -240,78 +178,36 @@ namespace Jace
             return BuildFormula(formulaText, compiledConstants,  operation);
         }
 
-        /// <summary>
-        /// Add a function to the calculation engine.
-        /// </summary>
-        /// <param name="functionName">The name of the function. This name can be used in mathematical formulas.</param>
-        /// <param name="function">The implementation of the function.</param>
-        /// <param name="isIdempotent">Does the function provide the same result when it is executed multiple times.</param>
         public void AddFunction(string functionName, Func<double> function, bool isIdempotent = true)
         {
             FunctionRegistry.RegisterFunction(functionName, function, isIdempotent, true);
         }
 
-        /// <summary>
-        /// Add a function to the calculation engine.
-        /// </summary>
-        /// <param name="functionName">The name of the function. This name can be used in mathematical formulas.</param>
-        /// <param name="function">The implementation of the function.</param>
-        /// <param name="isIdempotent">Does the function provide the same result when it is executed multiple times.</param>
         public void AddFunction(string functionName, Func<double, double> function, bool isIdempotent = true)
         {
             FunctionRegistry.RegisterFunction(functionName, function, isIdempotent, true); 
         }
 
-        /// <summary>
-        /// Add a function to the calculation engine.
-        /// </summary>
-        /// <param name="functionName">The name of the function. This name can be used in mathematical formulas.</param>
-        /// <param name="function">The implementation of the function.</param>
-        /// <param name="isIdempotent">Does the function provide the same result when it is executed multiple times.</param>
         public void AddFunction(string functionName, Func<double, double, double> function, bool isIdempotent = true)
         {
             FunctionRegistry.RegisterFunction(functionName, function, isIdempotent, true);
         }
 
-        /// <summary>
-        /// Add a function to the calculation engine.
-        /// </summary>
-        /// <param name="functionName">The name of the function. This name can be used in mathematical formulas.</param>
-        /// <param name="function">The implementation of the function.</param>
-        /// <param name="isIdempotent">Does the function provide the same result when it is executed multiple times.</param>
         public void AddFunction(string functionName, Func<double, double, double, double> function, bool isIdempotent = true)
         {
             FunctionRegistry.RegisterFunction(functionName, function, isIdempotent, true);
         }
 
-        /// <summary>
-        /// Add a function to the calculation engine.
-        /// </summary>
-        /// <param name="functionName">The name of the function. This name can be used in mathematical formulas.</param>
-        /// <param name="function">The implementation of the function.</param>
-        /// <param name="isIdempotent">Does the function provide the same result when it is executed multiple times.</param>
         public void AddFunction(string functionName, Func<double, double, double, double, double> function, bool isIdempotent = true)
         {
             FunctionRegistry.RegisterFunction(functionName, function, isIdempotent, true);
         }
 
-        /// <summary>
-        /// Add a function to the calculation engine.
-        /// </summary>
-        /// <param name="functionName">The name of the function. This name can be used in mathematical formulas.</param>
-        /// <param name="function">The implementation of the function.</param>
-        /// <param name="isIdempotent">Does the function provide the same result when it is executed multiple times.</param>
         public void AddFunction(string functionName, Func<double, double, double, double, double, double> function, bool isIdempotent = true)
         {
             FunctionRegistry.RegisterFunction(functionName, function, isIdempotent, true);
         }
 
-        /// <summary>
-        /// Add a function to the calculation engine.
-        /// </summary>
-        /// <param name="functionName">The name of the function. This name can be used in mathematical formulas.</param>
-        /// <param name="function">The implementation of the function.</param>
-        /// <param name="isIdempotent">Does the function provide the same result when it is executed multiple times.</param>
         public void AddFunction(string functionName, Func<double, double, double, double, double, double, double> function, bool isIdempotent = true)
         {
             FunctionRegistry.RegisterFunction(functionName, function, isIdempotent, true);
@@ -372,11 +268,6 @@ namespace Jace
             FunctionRegistry.RegisterFunction(functionName, functionDelegate, isIdempotent, true);
         }
 
-        /// <summary>
-        /// Add a constant to the calculation engine.
-        /// </summary>
-        /// <param name="constantName">The name of the constant. This name can be used in mathematical formulas.</param>
-        /// <param name="value">The value of the constant.</param>
         public void AddConstant(string constantName, double value)
         {
             ConstantRegistry.RegisterConstant(constantName, value);
